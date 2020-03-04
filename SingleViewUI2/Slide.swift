31a8752e22dc6d33faa80c251cbb449f38fea885
fawwazmayda <maydafawwaz@gmail.com>
2020-03-04 19:23:50 +0700

    Adding Slide.swift and Slide.xib

---
+++ SingleViewUI2/Slide.swift
@@ -0,0 +1,21 @@
+//
+//  Slide.swift
+//  SingleViewUI2
+//
+//  Created by Muhammad Fawwaz Mayda on 04/03/20.
+//  Copyright © 2020 Muhammad Fawwaz Mayda. All rights reserved.
+//
+
+import UIKit
+
+class Slide: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
