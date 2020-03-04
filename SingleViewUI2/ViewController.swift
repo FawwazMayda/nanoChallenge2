0151812c4f97a683905440f304a49966c432316b
fawwazmayda <maydafawwaz@gmail.com>
2020-03-04 19:18:44 +0700

    Initial Commit

---
+++ SingleViewUI2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SingleViewUI2
+//
+//  Created by Muhammad Fawwaz Mayda on 04/03/20.
+//  Copyright © 2020 Muhammad Fawwaz Mayda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
